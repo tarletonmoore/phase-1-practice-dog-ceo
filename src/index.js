dce457e3ebed7861538c639801a3acb280e6553a
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-04 16:51:08 -0400

    commit tree febde3137bd5dddd2f0a3424d7dcf7cee24dd602

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function dogImages() {
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(data => data.message.forEach(image => appendImage(image)))
+}
+console.log(dogImages())
+
+// document.getElementById("dog-image-container")
+function appendImage(imageUrl) {
+    let imageC = document.getElementById("dog-image-container")
+    let newElement = document.createElement("img")
+    newElement.src = imageUrl
+    imageC.append(newElement)
+}
